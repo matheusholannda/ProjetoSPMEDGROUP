2f1ad176297934fc2a69f6f3d06203a7c5818262
Matheus de Holanda Pereira Ribeiro <35711103822@info.local>
2019-02-12 15:56:14 -0200

    Projeto Finalizado

--- scripts/DML-MANIPULACAO-02.sql
+++ scripts/DML-MANIPULACAO-02.sql
@@ -8,3 +8,58 @@ FROM 'C:\db\ESPECIALIDADE.CSV'
 		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
 		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
 );
+
+BULK INSERT CONSULTAS
+FROM 'C:\db\CONSULTAS.CSV'
+	WITH(
+		FORMAT = 'csv',
+		FIRSTROW = 2, -- primeira linha de dados = 2 
+		FIELDTERMINATOR = ';', -- separador de campos = ';'
+		ROWTERMINATOR = '\n', -- separador de linhas = '\n'
+		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
+		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
+);
+
+BULK INSERT MEDICOS
+FROM 'C:\db\MEDICOS.CSV'
+	WITH(
+		FORMAT = 'csv',
+		FIRSTROW = 2, -- primeira linha de dados = 2 
+		FIELDTERMINATOR = ';', -- separador de campos = ';'
+		ROWTERMINATOR = '\n', -- separador de linhas = '\n'
+		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
+		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
+);
+
+BULK INSERT TIPO_USUARIOS
+FROM 'C:\db\TIPO_USUARIOS.CSV'
+	WITH(
+		FORMAT = 'csv',
+		FIRSTROW = 2, -- primeira linha de dados = 2 
+		FIELDTERMINATOR = ';', -- separador de campos = ';'
+		ROWTERMINATOR = '\n', -- separador de linhas = '\n'
+		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
+		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
+);
+
+BULK INSERT USUARIOS
+FROM 'C:\db\USUARIOS.CSV'
+	WITH(
+		FORMAT = 'csv',
+		FIRSTROW = 2, -- primeira linha de dados = 2 
+		FIELDTERMINATOR = ';', -- separador de campos = ';'
+		ROWTERMINATOR = '\n', -- separador de linhas = '\n'
+		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
+		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
+);
+
+BULK INSERT PRONTUARIO_PACIENTE
+FROM 'C:\db\PRONTUARIO_PACIENTE.CSV'
+	WITH(
+		FORMAT = 'csv',
+		FIRSTROW = 2, -- primeira linha de dados = 2 
+		FIELDTERMINATOR = ';', -- separador de campos = ';'
+		ROWTERMINATOR = '\n', -- separador de linhas = '\n'
+		CODEPAGE = 'ACP', -- codificação dos dados = 'ACP'
+		DATAFILETYPE = 'Char' -- tipo do arquivo = 'Char'
+);
